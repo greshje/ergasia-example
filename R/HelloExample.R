fd882778d44b3e40fc92703af4c9fda958232eb0
NACHC-CAD <greshje.nachc@gmail.com>
2023-12-17 21:56:40 -0500

    example is working for R6 class

---
+++ R/HelloExample.R
@@ -0,0 +1,88 @@
+# ---
+#
+# clear the environment and import dependencies
+#
+# ---
+
+rm(list = ls())
+library(devtools)
+
+# ---
+# 
+# remove functions
+#
+# ---
+
+pkgName = "ergasia"
+
+removeErgasia <- function() {
+  required <- requireNamespace(pkgName, quietly = TRUE)
+  print(paste(pkgName, required, sep = ": "))
+  if (required) {
+    remove.packages(pkgName)
+  }
+}
+
+forceRemoveErgasia <- function() {
+  tryCatch({
+    devtools::unload(pkgName)
+  }, error = function(e) {
+    writeLines(paste0("COULD NOT UNLOAD PACKAGE: ", pkgName))
+  })
+  tryCatch({
+    removeErgasia()
+  }, error = function(e) {
+    writeLines(paste0("COULD NOT REMOVE PACKAGE: ", pkgName))
+  })
+}
+
+# ---
+#
+# run this to remove the ergasia package
+#
+# ---
+
+forceRemoveErgasia()
+
+# ---
+#
+# this should give an error
+#
+# ---
+
+tryCatch({
+    library(ergasia)
+    stop("Library did not unload as expected.")
+  }, error = function(e) {
+    writeLines("Library did not load (this is expected).")
+  }
+)
+
+# ---
+#
+# this should load ergasia
+#
+# ---
+
+library(remotes)
+remotes::install_github("greshje/ergasia", ref="main", upgrade = TRUE, INSTALL_opts = "--no-multiarch")
+
+# ---
+#
+# this should now work
+# 
+# ---
+
+library(ergasia)
+
+ls("package:ergasia")
+
+ergasia::hello()
+ergasia::thankyou()
+
+libUtil <- ergasia::getNewOhdsiLibUtil()
+
+
+
+
+
